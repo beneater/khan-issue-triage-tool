b025327afb5ed36dee05faa7c605b8c876885576
Ben Eater <ben@eater.net>
2011-11-18 23:43:05 -0500

    Switch to APIv3; search for tags on client
    APIv2 isn't supported and the search is broken. GitHub suggested moving
    to APIv3. Conveniently APIv3 doesn't have a search feature :(  So this
    pulls down all the comments and searches them manually.
    
    GitHub limits API calls to 5000 per hour per client. This could be an
    issue with the number of issues open. In theory if I added oauth, they
    could lift that limit.

--- khan-issues.js
+++ khan-issues.js
@@ -1,140 +1,150 @@
-// Array Remove - By John Resig (MIT Licensed)
+// Array Remove & Pretty Date - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
 	var rest = this.slice((to || from) + 1 || this.length);
 	this.length = from < 0 ? this.length + from : from;
 	return this.push.apply(this, rest);
 };
 
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+//
+// hack added: it assumes time is UTC
+function prettyDate(time) {
+	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+		diff = (((new Date()).getTime() - date.getTime() + date.getTimezoneOffset() * 60000) / 1000),
+		day_diff = Math.floor(diff / 86400);
+
+	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+		return;
+
+	return day_diff == 0 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff < 7 && day_diff + " days ago" ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+}
+
+
+// tags listed first get matched first
+// "other" is special and should be last
+// to add a tag, also update the HTML and populateDom calls below
+var TAGS = [ "realbug", "notabug", "other" ];
+
 jQuery( function() {
 	jQuery( "#tabs" ).tabs();
 	jQuery( "#loading" ).fadeIn( 200 );
-	jQuery.getJSON( 'http://github.com/api/v2/json/issues/list/Khan/khan-exercises/open?callback=?', function( data ) {
-		newIssues = {};
-		commentIssues = {};
-		realbugIssues = {};
-		notabugIssues = {};
-		converter = new Showdown.converter();
+	var newIssues = {};
+	var taggedIssues = {};
+	jQuery.each( TAGS, function() {
+		taggedIssues[ this ] = {};
+	});
+	var converter = new Showdown.converter();
 
-		jQuery.each( data.issues, function() {
-			if ( this.user == "KhanBugz" ) {
-				var exerciseName = this.title.split(" - ")[0];
-				if ( this.comments > 0 ) {
-					if ( exerciseName in commentIssues ) {
-						commentIssues[ exerciseName ].push( this );
-					} else {
-						commentIssues[ exerciseName ] = [ this ];
-					}
-				} else {
-					if ( exerciseName in newIssues ) {
-						newIssues[ exerciseName ].push( this );
-					} else {
-						newIssues[ exerciseName ] = [ this ];
-					}
-				}
-			}
-		});
-
-		jQuery.getJSON( 'http://github.com/api/v2/json/issues/search/Khan/khan-exercises/open/realbug?callback=?', function( data ) {
-			jQuery.each( data.issues, function() {
-				if ( this.user == "KhanBugz" ) {
+	var getIssues = function( page ) {
+		var commentXHRs = [];
+		jQuery.getJSON( 'https://api.github.com/repos/Khan/khan-exercises/issues?page=' + page + '&per_page=100&callback=?', function( data ) {
+			jQuery( "#githublimit" ).text( data.meta[ "X-RateLimit-Remaining" ] );
+			jQuery.each( data.data, function() {
+				if ( this.user.login == "KhanBugz" ) {
+					var exercise = this;
 					var exerciseName = this.title.split(" - ")[0];
-					if ( exerciseName in realbugIssues ) {
-						realbugIssues[ exerciseName ].push( this );
+					if ( this.comments > 0 ) {
+						commentXHRs.push( jQuery.getJSON( 'https://api.github.com/repos/Khan/khan-exercises/issues/' + this.number + '/comments?per_page=100&callback=?', function( data ) {
+							jQuery( "#githublimit" ).text( data.meta[ "X-RateLimit-Remaining" ] );
+							var text = "";
+							jQuery.each( data.data, function() {
+								text += this.body;
+							});
+							jQuery.each( TAGS, function() {
+								if ( ( text.toLowerCase().search( this ) !== -1 ) || ( String(this) === "other" ) ) {
+									if ( exerciseName in taggedIssues[ this ] ) {
+										taggedIssues[ this ][ exerciseName ].push( exercise );
+									} else {
+										taggedIssues[ this ][ exerciseName ] = [ exercise ];
+									}
+									return false;
+								}
+							});
+						}));
 					} else {
-						realbugIssues[ exerciseName ] = [ this ];
-					}
-					var issueNum = this.number;
-					var removeIndex = -1;
-					jQuery.grep( commentIssues[ exerciseName ], function( element, index ) {
-						if ( element.number === issueNum ) {
-							removeIndex = index;
-						};
-					});
-					if ( removeIndex !== -1 ) {
-						commentIssues[ exerciseName ].remove( removeIndex );
+						if ( exerciseName in newIssues ) {
+							newIssues[ exerciseName ].push( this );
+						} else {
+							newIssues[ exerciseName ] = [ this ];
+						}
 					}
 				}
 			});
-
-			jQuery.getJSON( 'http://github.com/api/v2/json/issues/search/Khan/khan-exercises/open/notabug?callback=?', function( data ) {
-				jQuery.each( data.issues, function() {
-					if ( this.user == "KhanBugz" ) {
-						var exerciseName = this.title.split(" - ")[0];
-						if ( exerciseName in notabugIssues ) {
-							notabugIssues[ exerciseName ].push( this );
-						} else {
-							notabugIssues[ exerciseName ] = [ this ];
-						}
-						var issueNum = this.number;
-						var removeIndex = -1;
-						jQuery.grep( commentIssues[ exerciseName ], function( element, index ) {
-							if ( element.number === issueNum ) {
-								removeIndex = index;
-							};
+			jQuery.when.apply( jQuery, commentXHRs ).done(function() {
+				jQuery( "#loadprogress" ).text( "(" + page * 100 + ")" );
+				if ( data.meta.Link[0][1].rel === "next" ) {
+					getIssues( page + 1 );
+				} else {
+					var populateDom = function( issueList, container ) {
+						var sortedIssues = [];
+						var totalIssues = 0;
+						jQuery.each( issueList, function( exerciseName, value ) {
+							sortedIssues.push( exerciseName );
+							totalIssues += value.length;
 						});
-						if ( removeIndex !== -1 ) {
-							commentIssues[ exerciseName ].remove( removeIndex );
-						}
-					}
-				});
+						sortedIssues = sortedIssues.sort( function(a, b) { return issueList[ b ].length - issueList[ a ].length; });
 
-				var populateDom = function( issueList, container ) {
-					var sortedIssues = [];
-					var totalIssues = 0;
-					jQuery.each( issueList, function( exerciseName, value ) {
-						sortedIssues.push( exerciseName );
-						totalIssues += value.length;
-					});
-					sortedIssues = sortedIssues.sort( function(a, b) { return issueList[ b ].length - issueList[ a ].length; });
-
-					jQuery.each( sortedIssues, function() {
-						var exerciseName = this + "";
-						var joinedTitle = exerciseName.split(' ').join('');
-						var issue = jQuery( "#" + container + " .exercise-list" ).append( '<div id="' + joinedTitle + '" class="exercise-list-item">' )
-						jQuery( "#" + container + " #" + joinedTitle ).append('<span class="issue-count">');
-						jQuery( "#" + container + " #" + joinedTitle ).append('<span class="exercise-name">');
-						jQuery( "#" + container + " #" + joinedTitle + " .issue-count" ).append( issueList[ exerciseName ].length ) ;
-						jQuery( "#" + container + " #" + joinedTitle + " .exercise-name" ).append( exerciseName ) ;
-						jQuery( "#" + container + " #" + joinedTitle ).click( function( event ) {
-							event.preventDefault();
-							jQuery( "#" + container + " .exercise-list .exercise-list-item" ).removeClass( "highlighted" );
-							jQuery( this ).addClass( "highlighted" );
-							var detail = jQuery( "#" + container + " .issue-detail" )
-							detail.empty();
-							detail.scrollTop( 0 );
-							jQuery( "<h2>" ).append( exerciseName ).appendTo( detail );
-							jQuery.each( issueList[ exerciseName ], function() {
-								var issuediv = jQuery( "<div>" ).addClass("issue").appendTo( detail );
-								var issuelink = jQuery( "<a>" ).append( "#" + this.number + "&nbsp;&mdash;&nbsp;" + this.title.split(" - ")[1] ).appendTo( jQuery( issuediv ) );
-								issuelink.attr({
-									href: this.html_url,
-									target: "github"
-								});
-								var markdown = converter.makeHtml( this.body );
-								markdown = markdown.replace(/(AskTb[^\s\)]*)/, '<span style="color: #f00">$1</span>');
-								var markdownDiv = jQuery( '<div class="markdown">' ).append( markdown ).appendTo( issuediv );
-								if ( this.comments > 0 ) {
-									jQuery( '<a class="comment-count">' ).append( this.comments + " comment" + (this.comments === 1 ? "" : "s") ).appendTo( jQuery( issuediv ) ).attr({
+						jQuery.each( sortedIssues, function() {
+							var exerciseName = this + "";
+							var joinedTitle = exerciseName.split(' ').join('');
+							var issue = jQuery( "#" + container + " .exercise-list" ).append( '<div id="' + joinedTitle + '" class="exercise-list-item">' )
+							jQuery( "#" + container + " #" + joinedTitle ).append('<span class="issue-count">');
+							jQuery( "#" + container + " #" + joinedTitle ).append('<span class="exercise-name">');
+							jQuery( "#" + container + " #" + joinedTitle + " .issue-count" ).append( issueList[ exerciseName ].length ) ;
+							jQuery( "#" + container + " #" + joinedTitle + " .exercise-name" ).append( exerciseName ) ;
+							jQuery( "#" + container + " #" + joinedTitle ).click( function( event ) {
+								event.preventDefault();
+								jQuery( "#" + container + " .exercise-list .exercise-list-item" ).removeClass( "highlighted" );
+								jQuery( this ).addClass( "highlighted" );
+								var detail = jQuery( "#" + container + " .issue-detail" )
+								detail.empty();
+								detail.scrollTop( 0 );
+								jQuery( "<h2>" ).append( exerciseName ).appendTo( detail );
+								jQuery.each( issueList[ exerciseName ], function() {
+									var issuediv = jQuery( "<div>" ).addClass("issue").appendTo( detail );
+									var issuelink = jQuery( "<a>" ).append( "#" + this.number + "&nbsp;&mdash;&nbsp;" + this.title.split(" - ")[1] ).appendTo( jQuery( issuediv ) );
+									issuelink.attr({
 										href: this.html_url,
 										target: "github"
 									});
-								}
+									jQuery( issuediv ).append( "<br />" + ( prettyDate( this.created_at ) || this.created_at ) );
+									var markdown = converter.makeHtml( this.body );
+									markdown = markdown.replace(/(AskTb[^\s\)]*)/i, '<span style="color: #f00">$1</span>');
+									var markdownDiv = jQuery( '<div class="markdown">' ).append( markdown ).appendTo( issuediv );
+									if ( this.comments > 0 ) {
+										jQuery( '<a class="comment-count">' ).append( this.comments + " comment" + (this.comments === 1 ? "" : "s") ).appendTo( jQuery( issuediv ) ).attr({
+											href: this.html_url,
+											target: "github"
+										});
+									}
+								});
+								jQuery( ".markdown a" ).attr( "target", "sandcastle" );
 							});
-							jQuery( ".markdown a" ).attr( "target", "sandcastle" );
 						});
-					});
-					jQuery( "<span>" ).append( " (" + totalIssues + ") " ).appendTo( jQuery( "#tabs li a[href=#" + container + "]" ) );
-				};
-				populateDom( newIssues, "new-issues" );
-				populateDom( realbugIssues, "realbug-issues" );
-				populateDom( notabugIssues, "notabug-issues" );
-				populateDom( commentIssues, "comment-issues" );
-				jQuery( ".exercise-list-item:first-child" ).trigger( 'click' );
-				jQuery( "#loading" ).fadeOut( 200 );
+						jQuery( "<span>" ).append( " (" + totalIssues + ") " ).appendTo( jQuery( "#tabs li a[href=#" + container + "]" ) );
+					};
+					populateDom( newIssues, "new-issues" );
+					populateDom( taggedIssues.realbug, "realbug-issues" );
+					populateDom( taggedIssues.notabug, "notabug-issues" );
+					populateDom( taggedIssues.other, "comment-issues" );
+					jQuery( ".exercise-list-item:first-child" ).trigger( 'click' );
+					jQuery( "#loading" ).fadeOut( 200 );
+				}
 			});
+
 		});
-	});
-})
+	}
+	getIssues( 1 );
+});
 
 jQuery( document ).ready( function( event ) {
 	jQuery( window ).trigger( 'resize' );
